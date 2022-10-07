9a6917852eb00755c1fa35af29422a7e69f73250
MikelAlejoBR <mbarcina@redhat.com>
2022-10-07 09:50:31 +0200

    Initial commit

---
+++ src/main/java/org/acme/GreetingResource.java
@@ -0,0 +1,18 @@
+package org.acme;
+
+import javax.annotation.security.RolesAllowed;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/hello")
+public class GreetingResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    @RolesAllowed("events:read")
+    public String hello() {
+        return "Hello RESTEasy";
+    }
+}
